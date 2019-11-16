326045b3697244ab531c823c6a420904465d1852
Đỗ Tường Hoàng Phú <55187582+dophu1402@users.noreply.github.com>
2019-11-16 22:07:38 +0700

    Revert "Revert "Add files via upload""
    This reverts commit 2aa0725ddb0d3b18775bf87dd259536aa46cf601.

---
+++ ConsoleApplication8/Header.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include "Paddle.h"
+#include "Ball.h"
+#include "Wall.h"
+#include "Menu.h"
+#include <string>
+#include <ctime>
+
+void defaultPos(Paddle& paddle1, Paddle& paddle2, Ball& ball); //đưa bảng đỡ, bóng về vị trí ban đầu
+void drawToWindow(sf::RenderWindow& window, Paddle paddle1, Paddle paddle2, Ball ball, Wall wall, sf::Text Exit); //thao tác vẽ bảng, tường, bóng
+void changeSpeed(sf::RenderWindow& window, sf::Event& event, sf::Text& Level, int& speed,Menu); //tăng giảm tốc độ
+void changeSetting(sf::RenderWindow& window, sf::Event& event, sf::Text& Level, float& defaulSpeed, float& ballSpeed, float& padSpeed, int& speed, bool& AIMode, sf::Text & Mode); //thay đổi Mode, Speed
+void changeMode(sf::RenderWindow& window, sf::Event& event, Menu optionScreen, bool& AImode, sf::Text &Mode); //đổi chế 1 player hoặc 2 player
+void gamePlay(sf::RenderWindow& window, sf::Font font, float padSpeed, float ballSpeed, float defaultSpeed, bool& playing, bool AIMode); //Thực hiện các hoạt động của game
+void result(int scoreLeft, int scoreRight, sf::Text& Result); //thống kê điểm
---
+++ ConsoleApplication8/Paddle.cpp
@@ -0,0 +1,47 @@
+﻿#include "Paddle.h"
+
+Paddle::Paddle() {
+	paddle.setSize({ 0,0 });//khởi tạo size mặc định = 0
+}
+
+void Paddle::setSize(sf::Vector2f size) {
+	paddle.setSize(size);//cài size
+}
+
+Paddle::~Paddle() {
+
+}
+
+Paddle::Paddle(sf::Vector2f size) {
+	paddle.setSize(size);//khởi tạo với size cho trước
+}
+
+void Paddle::drawTo(sf::RenderWindow& window) {
+	window.draw(paddle);//vẽ thanh trượt (hình chữ nhật)
+}
+
+void Paddle::move(sf::Vector2f distance) {
+	paddle.move(distance);//di chuyển theo hướng vector
+}
+
+void Paddle::setPos(sf::Vector2f newPos) {
+	paddle.setPosition(newPos);//cài đặt vị trí
+}
+
+void Paddle::setColor(int a, int b, int c, int d) {
+	paddle.setFillColor(sf::Color(a, b, c, d));//đổi màu
+}
+
+sf::Vector2f Paddle::getPos() {
+	return paddle.getPosition(); // lấy vị trí
+}
+
+bool Paddle::isHitBall(Ball ball) {
+	//nếu bóng chạm thanh trượt thì trả về true
+	if (ball.getPos().y + ball.getRad() * 2 > paddle.getPosition().y && ball.getPos().y < paddle.getPosition().y + paddle.getSize().y) {
+		if (ball.getPos().x + ball.getRad() * 2 > paddle.getPosition().x && ball.getPos().x < paddle.getPosition().x + paddle.getSize().x) {
+			return true;
+		}
+	}
+	return false;
+}
